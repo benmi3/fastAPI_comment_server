aa4bf534819b5b5a18231885f92dff7afd0804c5
Benjamin Jørgensen <me@benmi.me>
2023-12-31 23:06:27 +0900

    added some get post defs, also created a check db connection at in the main.py

--- comment/comment.py
+++ comment/comment.py
@@ -1,14 +1,48 @@
-# Note: the module name is psycopg, not psycopg3
+import os
 import psycopg
 
+db_user = os.environ.get('POSTGRES_USER')
+db_pass = os.environ.get('POSTGRES_PASSWORD')
+db_host = os.environ.get('POSTGRES_HOST')
+db_name = os.environ.get('POSTGRES_DB')
+db_port = os.environ.get('POSTGRES_PORT')
+db_url = f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
 
-def insert_comment():
+
+def check_db_connection() -> bool:
+    try:
+        connection = psycopg.connect(db_url)
+        cursor = connection.cursor()
+        cursor.close()
+        connection.close()
+        return True
+    except psycopg.DatabaseError as error:
+        return False
+
+
+def setup_comment_table():
     # Connect to an existing database
-    with psycopg.connect("dbname=test user=postgres") as conn:
-
+    with psycopg.connect(db_url) as conn:
         # Open a cursor to perform database operations
         with conn.cursor() as cur:
+            # Execute a command: this creates a new table
+            cur.execute("""
+                    CREATE TABLE comment_table (
+                        id serial PRIMARY KEY,
+                        cid integer,
+                        comment text,
+                        post_slug text)
+                        IF NOT EXISTS
+                    """)
 
+            conn.commit()
+
+
+def insert_comment(comment: str, post: str, author: str):
+    # Connect to an existing database
+    with psycopg.connect(db_url) as conn:
+        # Open a cursor to perform database operations
+        with conn.cursor() as cur:
             # Execute a command: this creates a new table
             cur.execute("""
                 CREATE TABLE test (
@@ -24,8 +58,7 @@ def insert_comment():
                 (100, "abc'def"))
 
             # Query the database and obtain data as Python objects.
-            cur.execute("SELECT * FROM test")
-            cur.fetchone()
+            cur.execute("SELECT * FROM test").fetchall()
             # will return (1, 100, "abc'def")
 
             # You can use `cur.fetchmany()`, `cur.fetchall()` to return a list
@@ -35,3 +68,16 @@ def insert_comment():
 
             # Make the changes to the database persistent
             conn.commit()
+
+
+async def select_comments(post: str):
+    async with await psycopg.AsyncConnection.connect(db_url) as aconn:
+        async with aconn.cursor() as acur:
+            await acur.execute(
+                "INSERT INTO test (num, data) VALUES (%s, %s)",
+                (100, "abc'def"))
+            await acur.execute("SELECT * FROM test")
+            await acur.fetchall()
+            # will return (1, 100, "abc'def")
+            async for record in acur:
+                print(record)
--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from typing import Union
+from datetime import datetime
 from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+# Local packages
+import comment.comment as comment
+
+
+class CommentJson(BaseModel):
+    text: str
+
 
 app = FastAPI()
 
@@ -17,3 +27,22 @@ async def say_hello(name: str):
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/comment/{post}/")
+def get_comment(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.post("/comment/{post}/")
+def post_comment(post: str, item: CommentJson):
+    json_compatible_item_data = jsonable_encoder(item)
+    print(post)
+    print(json_compatible_item_data)
+
+
+if __name__ == "__main__":
+    if not comment.check_db_connection():
+        print("Could not get connection to database")
+        print("Check environmental variables")
+        exit()
