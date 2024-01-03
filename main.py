195042d3ea12092af5bc8f9c5e17abbec07c17f8
Benjamin Jørgensen <me@benmi.me>
2024-01-03 16:24:41 +0900

    outlaying the application more. Still need to part it up into routes, but that will be the next faze

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
-from typing import Union
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
+from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 # Local packages
 import models
 
 
-class CommentJson(BaseModel):
+class CommentCreate(BaseModel):
     text: str
+
+
+class CommentDelete(BaseModel):
+    id: int
+
+
+class UserDisplay(BaseModel):
+    text: str
+
+
+class UserCreate(BaseModel):
+    text: str
+
+
+class UserUpdate(BaseModel):
+    text: str
+    id: int
+
+
+class UserDelete(BaseModel):
+    id: int
 
 
 app = FastAPI()
@@ -15,28 +36,61 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    # TODO: redirect to desired link?
+    # there should be no reason to call root.
+    url = "https://google.com"
+    return RedirectResponse(
+        url, status_code=307, headers=None, background=None
+    )
 
 
 @app.get("/comment/{post}/")
-async def get_comment(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+async def get_comment(post: str):
+    # This gets all the comments.
+    return {"post_id": post}
 
 
-@app.post("/comment/{post}/")
-async def post_comment(post: str, item: CommentJson):
+@app.put("/comment/{post}/")
+async def put_comment(post: str, item: CommentCreate):
+    # This creates the comment
     json_compatible_item_data = jsonable_encoder(item)
     print(post)
+    print(json_compatible_item_data)
+
+
+@app.delete("/comment/{post}/")
+async def delete_comment(post: str, item: CommentDelete):
+    # This deletes the comment
+    json_compatible_item_data = jsonable_encoder(item)
+    print(post)
+    print(json_compatible_item_data)
+
+
+@app.get("/user/")
+async def get_user(item: UserDisplay):
+    # This gets the info of the user that made the comment
+    json_compatible_item_data = jsonable_encoder(item)
+    print(json_compatible_item_data)
+
+
+@app.put("/user/")
+async def put_user(item: UserCreate):
+    # This creates a new user
+    json_compatible_item_data = jsonable_encoder(item)
+    print(json_compatible_item_data)
+
+
+@app.post("/user/")
+async def post_user(item: UserUpdate):
+    # This will update the user
+    json_compatible_item_data = jsonable_encoder(item)
+    print(json_compatible_item_data)
+
+
+@app.delete("/user/")
+async def delete_user(item: UserDelete):
+    # This will delete the user
+    json_compatible_item_data = jsonable_encoder(item)
     print(json_compatible_item_data)
 
 
